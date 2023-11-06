440fd16e70cad343b8cde571300913f6c6e8ee57
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-11-05 20:20:55 -0600

    updated about and added cards for my projects

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -1,22 +1,63 @@
-export default function Blog() {
+import React from 'react';
+
+export default function Home() {
   return (
-    <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    <div className="card-group">
+      <section className="left-side" id="Projects">
+        <h1 className="title">Projects</h1>
+        <section className="section-container">
+          <section id="image1">
+            <a href="https://ambitiousflo.github.io/Sports-Trivia/">
+              <img
+                src="./assets/images/Screenshot 2023-08-01 193531.png"
+                alt="Sports-Trivia"
+                id="p1"
+              />
+            </a>
+            <h3>Sports Trivia</h3>
+          </section>
+          <div className="card">
+            <a href="https://taskmaster-grp3-ef54a6c054dc.herokuapp.com/login">
+              <img
+                src="./assets/images/Screenshot 2023-10-02 013249.png"
+                className="card-img-top"
+                alt="..."
+              />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">Task Master</h5>
+              <p className="card-text">
+                Task Master is a web application that simplifies task
+                management. It provides a user-friendly interface for creating,
+                editing, updating, and deleting tasks, helping you stay
+                organized and focused on the most important tasks that need to
+                be done.
+              </p>
+              <p className="card-text">
+                <small className="text-body-secondary">
+                  Last updated 3 mins ago
+                </small>
+              </p>
+            </div>
+          </div>
+          <section className="card">
+            <img src="https://placehold.co/600x400" alt="" />
+            <h3>image</h3>
+          </section>
+          <section className="card">
+            <img src="https://placehold.co/600x400" alt="" />
+            <h3>image</h3>
+          </section>
+          <section className="card">
+            <img src="https://placehold.co/600x400" alt="" />
+            <h3>image</h3>
+          </section>
+          <section className="card">
+            <img src="https://placehold.co/600x400" alt="" />
+            <h3>image</h3>
+          </section>
+        </section>
+      </section>
     </div>
   );
 }
