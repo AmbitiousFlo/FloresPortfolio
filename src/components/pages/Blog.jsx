63b3b7e20fc47c3d1d98760acd4a9f98186c0eb5
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-11-06 20:34:54 -0600

    file restructure

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -4,27 +4,27 @@ import { Container, Row, Col, Card } from 'react-bootstrap';
 const projects = [
   {
     title: 'Task Master',
-    imageSrc: 'assets/taskmaster.png',
+    imageSrc: 'public/assets/taskmaster.png',
     link: 'https://taskmaster-grp3-ef54a6c054dc.herokuapp.com/',
   },
   {
     title: 'Sports Trivia',
-    imageSrc: 'assets/sportstrivia.png',
+    imageSrc: 'public/assets/sportstrivia.png',
     link: 'https://ambitiousflo.github.io/Sports-Trivia/',
   },
   {
     title: 'Weather Dashboard',
-    imageSrc: 'assets/weatherdashboard.png',
+    imageSrc: 'public/assets/weatherdashboard.png',
     link: 'https://ambitiousflo.github.io/WeatherDashboard/',
   },
   {
     title: 'Project 4',
-    imageSrc: 'assets/comingsoon.png',
+    imageSrc: 'public/assets/comingsoon.png',
     link: 'https://example.com/project4',
   },
   {
     title: 'Project 5',
-    imageSrc: 'assets/comingsoon.png',
+    imageSrc: 'public/assets/comingsoon.png',
     link: 'https://example.com/project5',
   },
 ];
