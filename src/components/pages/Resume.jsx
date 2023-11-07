34079299d186b70ba42202b8c13f518f432e3056
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-11-06 18:05:04 -0600

    added resume.css to style my resume page

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap'; // Import Bootstrap Card and Button components
+import '../css/resume.css'; // Create a CSS file for styling
 
 export default function Resume() {
-  const resumeURL = '/path-to-your-resume.pdf'; // Replace with the actual path to your resume PDF
+  const resumeURL = 'assets/resume.pdf'; // Replace with the actual path to your resume PDF
 
   return (
-    <div className="container mt-5">
-      <h1>My Resume</h1>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="/path-to-your-resume-preview-image.jpg" alt="Resume Preview" />
-        <Card.Body>
-          <Card.Title>Preview</Card.Title>
-          <Card.Text>
-            Here's a preview of my resume. Click the button below to download the full version.
-          </Card.Text>
-          <Button variant="primary" href={resumeURL} download="your-resume.pdf">
-            Download Resume
-          </Button>
-        </Card.Body>
-      </Card>
+    <div className="container mt-5 d-flex justify-content-center align-items-center">
+      <div className="resume-card">
+        <h1>My Resume</h1>
+        <Card style={{ width: '24rem' }}>
+          <Card.Img variant="top" src="assets/resumeimg.png" alt="Resume Preview" />
+          <Card.Body>
+            <Card.Title>Preview</Card.Title>
+            <Card.Text>
+              Here's a preview of my resume. Click the button below to download the full version.
+            </Card.Text>
+            <Button variant="primary" href={resumeURL} download="Christian's Resume">
+              Download Resume
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
